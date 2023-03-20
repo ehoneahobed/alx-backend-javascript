2ae1ec31845afcd5a3ec37f1473a099d0efa9583
ehoneahobed <ehoneahobed@hotmail.com>
2023-03-20 18:05:54 +0000

    Add solution to task 2

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,7 @@
+/* eslint-disable new-cap */
+/* eslint-disable import/extensions */
+import getNeighborhoodsList from './2-arrow.js';
+
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
