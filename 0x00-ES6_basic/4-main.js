fca3250973f227e67e3c5999ea1762776d55627a
ehoneahobed <ehoneahobed@hotmail.com>
2023-03-20 20:02:43 +0000

    Add solution to task 4

---
+++ 0x00-ES6_basic/4-main.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/extensions */
+import returnHowManyArguments from './4-rest-parameter.js';
+
+console.log(returnHowManyArguments('one'));
+console.log(returnHowManyArguments('one', 'two', 3, '4th'));
