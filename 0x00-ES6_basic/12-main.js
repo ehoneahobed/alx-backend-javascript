0ca4899c19a98d64b40375d0ff1220f74a94152e
ehoneahobed <ehoneahobed@hotmail.com>
2023-03-20 23:40:21 +0000

    Add solution to task 12

---
+++ 0x00-ES6_basic/12-main.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/extensions */
+import createEmployeesObject from './11-createEmployeesObject.js';
+import createReportObject from './12-createReportObject.js';
+
+const employees = {
+  ...createEmployeesObject('engineering', ['Bob', 'Jane']),
+  ...createEmployeesObject('marketing', ['Sylvie']),
+};
+
+const report = createReportObject(employees);
+console.log(report.allEmployees);
+console.log(report.getNumberOfDepartments(report.allEmployees));
