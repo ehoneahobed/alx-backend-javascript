bcb7c085136c9622a88aa47adba78aa55f670b8c
ehoneahobed <ehoneahobed@hotmail.com>
2023-03-20 18:16:39 +0000

    Add solution to task 3

---
+++ 0x00-ES6_basic/3-main.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/extensions */
+import getSumOfHoods from './3-default-parameter.js';
+
+console.log(getSumOfHoods(34));
+console.log(getSumOfHoods(34, 3));
+console.log(getSumOfHoods(34, 3, 4));
