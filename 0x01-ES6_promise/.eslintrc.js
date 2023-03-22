27878a633b685aa91d13a57ef38221cbbe5ab2a2
ehoneahobed <ehoneahobed@hotmail.com>
2023-03-22 22:11:12 +0000

    Update formatting for solution to task 1

--- 0x01-ES6_promise/.eslintrc.js
+++ 0x01-ES6_promise/.eslintrc.js
@@ -1,35 +1,15 @@
 module.exports = {
-	env: {
-	  browser: false,
-	  es6: true,
-	  jest: true,
-	},
-	extends: [
-	  'airbnb-base',
-	  'plugin:jest/all',
-	],
-	globals: {
-	  Atomics: 'readonly',
-	  SharedArrayBuffer: 'readonly',
-	},
-	parserOptions: {
-	  ecmaVersion: 2018,
-	  sourceType: 'module',
-	},
-	plugins: ['jest'],
-	rules: {
-	  'no-console': 'off',
-	  'no-shadow': 'off',
-	  'no-restricted-syntax': [
-		'error',
-		'LabeledStatement',
-		'WithStatement',
-	  ],
-	},
-	overrides:[
-	  {
-		files: ['*.js'],
-		excludedFiles: 'babel.config.js',
-	  }
-	]
-  };
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
