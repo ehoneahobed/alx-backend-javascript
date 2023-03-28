fada68f63aed2300198fad1e81a4d67a34680a3c
ehoneahobed <ehoneahobed@hotmail.com>
2023-03-28 23:48:28 +0000

    Add solution to task 11

---
+++ 0x03-ES6_data_manipulation/100-main.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/extensions */
+import { queryAPI, weakMap } from './100-weak.js';
+
+const endpoint = { protocol: 'http', name: 'getUsers' };
+weakMap.get(endpoint);
+
+queryAPI(endpoint);
+console.log(weakMap.get(endpoint));
+
+queryAPI(endpoint);
+console.log(weakMap.get(endpoint));
+
+queryAPI(endpoint);
+queryAPI(endpoint);
+queryAPI(endpoint);
+queryAPI(endpoint);
---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,21 @@
+// create a new WeakMap instance
+export const weakMap = new WeakMap();
+
+// function to query an API endpoint
+export function queryAPI(endpoint) {
+  // initialize the number of API calls to 0
+  let numCalls = 0;
+
+  // if the endpoint already exists in the map, get the number of previous calls
+  if (weakMap.has(endpoint)) {
+    numCalls = weakMap.get(endpoint);
+  }
+
+  // increment the number of API calls for the endpoint by 1 and update the map
+  weakMap.set(endpoint, numCalls + 1);
+
+  // if the number of API calls for the endpoint exceeds 5, throw an error
+  if (numCalls + 1 > 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
