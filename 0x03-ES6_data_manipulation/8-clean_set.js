d71c8cc5e0e49610662e9ec61e0e41243c080093
ehoneahobed <ehoneahobed@hotmail.com>
2023-03-28 22:52:03 +0000

    Add solution to task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+/* eslint-disable array-callback-return */
+export default function cleanSet(set, string) {
+  if (string === undefined || string.length === 0) {
+    return '';
+  }
+  return [...set]
+    .filter((str) => (str !== undefined ? str.startsWith(string) : ''))
+    .map((str) => (str !== undefined ? str.slice(string.length) : ''))
+    .join('-');
+}
---
+++ 0x03-ES6_data_manipulation/8-main.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/extensions */
+import cleanSet from './8-clean_set.js';
+
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+// console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
