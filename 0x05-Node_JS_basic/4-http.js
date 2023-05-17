5851b04ab5e961819b1c47438311d8565bde477e
ehoneahobed <ehoneahobed@hotmail.com>
2023-05-17 00:46:53 +0000

    Add solution to task 3 and 4

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const host = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, host, () => {});
+
+module.exports = app;
